f418b28b6d3bd115717093ebfc374199c31954bb
Fidoi <elautoricoi@gmail.com>
2023-12-19 18:48:08 -0300

    Switch funcionando

---
+++ src/components/ThemeSwitcher.tsx
@@ -0,0 +1,38 @@
+'use client';
+
+import React, { useEffect, useState } from 'react';
+import { Switch } from '@nextui-org/react';
+import { MoonIcon } from './icons/MoonIcon';
+import { SunIcon } from './icons/SunIcon';
+import { useTheme } from 'next-themes';
+
+export default function ThemeSwitcher() {
+  const { theme, setTheme } = useTheme();
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  const handleToggleTheme = () => {
+    setTheme(theme === 'dark' ? 'light' : 'dark');
+  };
+
+  if (!isMounted) return null;
+
+  return (
+    <Switch
+      defaultSelected={theme === 'dark' ? false : true}
+      size='lg'
+      color='default'
+      onChange={handleToggleTheme}
+      thumbIcon={({ isSelected, className }) =>
+        isSelected ? (
+          <SunIcon className={className} />
+        ) : (
+          <MoonIcon className={className} />
+        )
+      }
+    ></Switch>
+  );
+}
